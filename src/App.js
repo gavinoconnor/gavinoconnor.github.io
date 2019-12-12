ada81bd4cf6bbbaa530f4ab42e78ba109511598f
gavinoconnor <gedmund@gmail.com>
2019-12-11 21:15:24 -0500

    add initial components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 
 // Components
 import Navbar from './Navbar/Navbar';
+import Blogs from './components/Blogs/Blogs';
+import Projects from './components/Projects/Projects';
+
 
 function App() {
   return (
     <div className="App">
+      <h1>Under Construction</h1>
       <Navbar />
-    <h1>Under Construction</h1>
+      <Blogs />
+      <Projects />
     </div>
   );
 }
