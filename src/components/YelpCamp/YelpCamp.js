91fde61dc85efd921bceea26cd6874f65f32bca4
gavinoconnor <gedmund@gmail.com>
2020-02-19 17:53:43 -0500

    add pic template to projects

--- src/components/YelpCamp/YelpCamp.js
+++ src/components/YelpCamp/YelpCamp.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import classes from './YelpCamp.module.css';
+import yelpcamp from './yelpcamp.jpeg';
+
 const yelpCamp = () => {
   return (
     <div>
       <h1>YelpCamp</h1>
       <div>
         <div>
+          <img src={yelpcamp} alt="yelpcamp" className={classes.Camp}/>
           <h3>About the project:</h3>
           <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
         </div>
