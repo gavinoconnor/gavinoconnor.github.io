a8cfb20d451a93dad90a23567de2ab7a48f27266
gavinoconnor <gedmund@gmail.com>
2020-02-24 15:04:41 -0500

    add custom font

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,18 +6,30 @@ import profile from './profile_pic_1.jpeg';
 const about = () => {
   return (
     <div className={classes.About}>
-      <h1>About Me</h1>
-      <img src={profile} alt="profile" className={classes.profile} />
-      <p>
-        Full Stack Web Developer possessing 10+ years of diverse technical,
-        marketing, and entrepreneurial experience with a passion for coding.
-        Flatiron School graduate, adept at building fully-functioning
-        applications.
-        <br /> Extensive cross-functional experience shepherding projects
-        through their lifecycle. An adaptable, creative problem solver with
-        excellent communication and interpersonal skills. Independently
-        motivated to stay current with relevant software and applications.
-      </p>
+      <div>
+        <div className={classes.Hello}>
+          <p>Hello.
+          <br />
+          A headline here.
+          <br />
+          A resume here.
+          </p>
+        </div>
+        <div className={classes.Intro}>
+          <p>
+            Full Stack Web Developer possessing 10+ years of diverse technical,
+            marketing, and entrepreneurial experience with a passion for coding.
+            Flatiron School graduate, adept at building fully-functioning
+            applications. Extensive cross-functional experience shepherding projects
+            through their lifecycle. An adaptable, creative problem solver with
+            excellent communication and interpersonal skills. Independently
+            motivated to stay current with relevant software and applications.
+          </p>
+        </div>
+      </div>
+      <div className={classes.ImageContainer}>
+        <img src={profile} alt="profile" className={classes.profile} />
+      </div>
     </div>
   );
 };
