a382d2423eb5115bfe36209bbed6c0afb041445e
gavinoconnor <gedmund@gmail.com>
2020-03-12 13:16:23 -0400

    parse medium post content

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -3,17 +3,28 @@ import React from 'react';
 import classes from './BlogPost.module.css';
 
 const blogPost = (props) => {
-  // console.log("Post: ", props.blog)
+  console.log("Post: ", props.blog)
+
+const tagToText = (node) => {
+let tag = document.createElement('div');
+tag.innerHTML = node;
+node = tag.innerText;
+return node;
+};
+
+const shortenText = (text, start, maxLength) => {
+  return text.length > maxLength ?
+  text.slice(start, maxLength):
+  text;
+};
+
   return (
     <div className={classes.BlogPost}>
       <h2>{props.blog.title}</h2>
-      {/* <p>{props.blog.pubDate}</p> */}
-      {/* <p>{props.blog.categories}</p> */}
       <p className={classes.BlogParagraph}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        {'. . . ' + shortenText(tagToText(props.blog.content), 83, 300) + ' . . .'}
       </p>
       <a href={props.blog.link}>Read More<br />
-      {/* <img src={props.blog.thumbnail} alt="thumbnail"/> */}
       </a>
     </div>
   );
