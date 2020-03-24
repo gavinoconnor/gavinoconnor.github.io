c1a63a1a6c840a2043ed9bd60b83b2a5da91c6b8
gavinoconnor <gedmund@gmail.com>
2020-03-24 14:32:42 -0400

    add burger video embed

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -11,11 +11,11 @@ const burgerBuilder = () => {
       <p>TK.<br/><br/>Built with ...
         <br/>Styled with ...</p>
       <p>Find the code at Github
-        <a href="https://github.com/gavinoconnor/YelpCamp">here.</a>
+        <a href="https://github.com/gavinoconnor/YelpCamp"> here.</a>
       </p>
     </div>
     <div className={classes.Video}>
-      <iframe title="BurgerBuilder demo video" width="560" height="315" src="https://www.youtube.com/embed/1Kuyz3Cvla4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+      <iframe title="BurgerBuilder demo video" width="560" height="315" src="https://www.youtube.com/embed/xIrDxvx40Ys" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
     </div>
   </div>);
 };
