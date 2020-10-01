8cf5ba07341cbc1266c9c40477174bf2554e1747
gavinoconnor <gedmund@gmail.com>
2020-10-01 15:36:57 -0400

    update project descriptions

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -11,6 +11,7 @@ import tixtra from '../../assets/tixtra.png';
 
 const projects = () => (
   <div className={classes.Projects}>
+
       <div className={classes.ProjectEven}>
         <h3>Tixtra</h3>
         <p>
@@ -23,6 +24,7 @@ const projects = () => (
         {/* <NavLink to="/tixtra">LAUNCH</NavLink> */}
         <img src={tixtra} alt="tixtra"/>
       </div>
+
       <div className={classes.ProjectOdd}>
         <h3>Habit Trak</h3>
         <p>
@@ -33,15 +35,15 @@ const projects = () => (
         See the frontend code <a href="https://github.com/gavinoconnor/habit-trak-client">here</a>. <br />
         See the backend code <a href="https://github.com/gavinoconnor/habit-trak-api">here</a>.
         </p>
-        {/* <NavLink to="/habit">LAUNCH</NavLink> */}
         <iframe title="HabitTrackr demo video" width="560" height="315" src="https://www.youtube.com/embed/0I3epQ6T_Yw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowfullscreen"></iframe>
       </div>
+
       <div className={classes.ProjectEven}>
         <h3>BurgerBuilder</h3>
         <p>
-        Burger Builder is a mobile-first application that replicates an online ordering app - for a burger! The <strong>backend</strong> was built with Ruby on Rails and Postgresql. The <strong>frontend</strong> was built with React and Bootstrap.
+        Burger Builder is a mobile-first application that replicates a meal-delivery app for customized burgers. The <strong>backend</strong> uses Axios and Firebase for data handling. The <strong>frontend</strong> was built with React and custom CSS.
         <br /><br />
-        Users select from a library of habits, define their goals, and record when a habit or goal has been completed. A simple chart illustrates their progress in real time.
+        Users select their toppings, confirm their order, and select between pickup or delivery.
         <br /><br />
         See the code <a href="https://github.com/gavinoconnor/burger-builder">here</a>.
         </p>
@@ -51,9 +53,7 @@ const projects = () => (
       <div className={classes.ProjectOdd}>
         <h3>YelpCamp</h3>
         <p>
-        YelpCamp is a mobile-first application that replicates an online ordering app - for a burger! The <strong>backend</strong> was built with Ruby on Rails and Postgresql. The <strong>frontend</strong> was built with React and Bootstrap.
-        <br /><br />
-        Users select from a library of habits, define their goals, and record when a habit or goal has been completed. A simple chart illustrates their progress in real time.
+        A Yelp-like application for uploading, rating, and reviewing campgrounds. The <strong>backend</strong> was built with Node.js, Express and MongoDB. The <strong>frontend</strong> was styled with Bootstrap.
         <br /><br />
         See the code <a href="https://github.com/gavinoconnor/YelpCamp">here</a>.
         </p>
