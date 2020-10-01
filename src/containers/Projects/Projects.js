da29eb9254447699ab9650b62eb7b466cfac88dc
gavinoconnor <gedmund@gmail.com>
2020-10-01 18:30:16 -0400

    add CSS rules for alternating projects

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// import { NavLink } from 'react-router-dom';
 import classes from './Projects.module.css';
 
 // import yelpcamp from '../../assets/yelpcamp.jpeg';
@@ -12,7 +11,7 @@ import tixtra from '../../assets/tixtra.png';
 const projects = () => (
   <div className={classes.Projects}>
 
-      <div className={classes.ProjectEven}>
+      <div className={classes.ProjectOdd}>
         <h3>Tixtra</h3>
         <p>
         Tixtra is a ticket-sharing application that connects users based on similar musical interests. The <strong>backend</strong> was built with Ruby on Rails, Postgresql, and the Ticketmaster API. The <strong>frontend</strong> was built with React-Redux and Material UI.
@@ -21,11 +20,10 @@ const projects = () => (
         <br /><br />
         See the code <a href="https://github.com/gavinoconnor/tixtra">here</a>.
         </p>
-        {/* <NavLink to="/tixtra">LAUNCH</NavLink> */}
         <img src={tixtra} alt="tixtra"/>
       </div>
 
-      <div className={classes.ProjectOdd}>
+      <div className={classes.ProjectEven}>
         <h3>Habit Trak</h3>
         <p>
         Habit Trak is a web application that helps users set goals and track their progress. The <strong>backend</strong> was built with Ruby on Rails and Postgresql. The <strong>frontend</strong> was built with React and Bootstrap.
@@ -38,7 +36,7 @@ const projects = () => (
         <iframe title="HabitTrackr demo video" width="560" height="315" src="https://www.youtube.com/embed/0I3epQ6T_Yw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowfullscreen"></iframe>
       </div>
 
-      <div className={classes.ProjectEven}>
+      <div className={classes.ProjectOdd}>
         <h3>BurgerBuilder</h3>
         <p>
         Burger Builder is a mobile-first application that replicates a meal-delivery app for customized burgers. The <strong>backend</strong> uses Axios and Firebase for data handling. The <strong>frontend</strong> was built with React and custom CSS.
@@ -47,17 +45,16 @@ const projects = () => (
         <br /><br />
         See the code <a href="https://github.com/gavinoconnor/burger-builder">here</a>.
         </p>
-        {/* <NavLink to="/burger">LAUNCH</NavLink> */}
         <iframe title="BurgerBuilder demo video" width="560" height="315" src="https://www.youtube.com/embed/xIrDxvx40Ys" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowfullscreen"></iframe>
       </div>
-      <div className={classes.ProjectOdd}>
+
+      <div className={classes.ProjectEven}>
         <h3>YelpCamp</h3>
         <p>
         A Yelp-like application for uploading, rating, and reviewing campgrounds. The <strong>backend</strong> was built with Node.js, Express and MongoDB. The <strong>frontend</strong> was styled with Bootstrap.
         <br /><br />
         See the code <a href="https://github.com/gavinoconnor/YelpCamp">here</a>.
         </p>
-        {/* <NavLink to="/yelpcamp">LAUNCH</NavLink> */}
         <iframe title="YelpCamp demo video" width="560" height="315" src="https://www.youtube.com/embed/1Kuyz3Cvla4" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
   </div>
