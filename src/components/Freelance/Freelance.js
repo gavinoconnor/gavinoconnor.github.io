d80667d060dbc7322ff6bd1763dbac1137af995e
gavinoconnor <gedmund@gmail.com>
2021-01-28 15:43:07 -0500

    add freelance section

---
+++ src/components/Freelance/Freelance.js
@@ -0,0 +1,12 @@
+import { faDivide } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+class Freelance extends React.Component {
+  render() {
+    return(
+      <div>Recent Freelance Work</div>
+    )
+  }
+}
+
+export default Freelance;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,10 +3,12 @@ import React from 'react';
 import About from '../About/About';
 import BlogsContainer from '../../containers/BlogsContainer/BlogsContainer';
 import Projects from '../../containers/Projects/Projects';
+import Freelance from '../Freelance/Freelance';
 
 const home = (props) => (
     <React.Fragment>
       <About />
+      <Freelance />
       <Projects />
       <BlogsContainer />
     </React.Fragment>
