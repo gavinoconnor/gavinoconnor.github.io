f2ddf64a22a670fe268a606b450fd05461fc7190
gavinoconnor <gedmund@gmail.com>
2021-01-29 16:59:18 -0500

    update App

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
 import Navbar from './containers/Navbar/Navbar';
-import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './containers/Projects/Projects';
 import BlogsContainer from './containers/BlogsContainer/BlogsContainer';
 import Footer from './components/Footer/Footer';
 
-import classes from './App.module.css';
+import './App.css';
 
 library.add(fab);
 
 class App extends React.Component {
   render() {
     return (
-      <div className={classes.App}>
+      <div className='app'>
         <Navbar />
-          {/* <Home /> */}
           <About />
           <Projects />
           <BlogsContainer />
