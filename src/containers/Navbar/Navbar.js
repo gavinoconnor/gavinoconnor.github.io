48d19f48a4912fbf24c9ee22a7f22d0b4445c2f7
gavinoconnor <gedmund@gmail.com>
2021-02-02 17:25:52 -0500

    change navbar to class component

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 import './Navbar.css';
 
-const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      {/* <div>GOC</div> */}
-      <ul className='nav-links'>
-        <li className='nav-item'><a href='#'>About</a></li>
-        <li className='nav-item'><a href='#'>Projects</a></li>
-        <li className='nav-item'><a href='#'>Blog</a></li>
-      </ul>
-    </nav>
-  );
+class Navbar extends React.Component {
+  testNav = () => {
+    console.log('clicket')
+  }
+
+  render() {
+    return (
+      <nav className='navbar'>
+        <ul className='nav-links'>
+          <li className='nav-item' id='about' onClick={this.testNav}><a href='#'>About</a></li>
+          <li className='nav-item' id='projects'><a href='#'>Projects</a></li>
+          <li className='nav-item' id='blog'><a href='#'>Blog</a></li>
+        </ul>
+      </nav>
+    )
+  }
 }
 
 export default Navbar;
