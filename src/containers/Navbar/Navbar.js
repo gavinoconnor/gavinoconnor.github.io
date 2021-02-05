fc32d60ea59163c2490025ff27b2669e332f6e47
gavinoconnor <gedmund@gmail.com>
2021-02-05 16:11:25 -0500

    style new nav buttons

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -9,8 +9,8 @@ class Navbar extends React.Component {
       <nav className='navbar'>
         <ul className='nav-links'>
           <li className='nav-item'>About</li>
-          <li className='nav-item'><button onClick={this.props.scroller}>Projects</button></li>
-          <li className='nav-item'><button onClick={this.props.scroller}>Blog</button></li>
+          <li className='nav-item'><button className='button' onClick={this.props.scroller}>Projects</button></li>
+          <li className='nav-item'><button className='button' onClick={this.props.scroller}>Blog</button></li>
         </ul>
       </nav>
     )
